12da22195147a92ab8ea68261ff66ddd26bc62a4
Zach <nicholasbond@stfrancis.edu>
2015-03-03 18:59:55 -0600

    Panel Changes
    Changed the panel from a push to an overlay and added the ability to
    slide out the panel by swiping the screen right.

--- platforms/ios/www/js/clickEvents.js
+++ platforms/ios/www/js/clickEvents.js
@@ -1,3 +1,16 @@
 $('.menuButton').on('tap',function(){
 	$('.menupanel').panel("open");
 });
+
+$( document ).on( "pageinit", function() {
+    $( document ).on( "swiperight", function( e ) {
+        // We check if there is no open panel on the page because otherwise
+        // a swipe to close the left panel would also open the right panel (and v.v.).
+        // We do this by checking the data that the framework stores on the page element (panel: open).
+        if ( $.mobile.activePage.jqmData( "panel" ) !== "open" ) {
+            if ( e.type === "swiperight"  ) {
+                $( '.menupanel' ).panel( "open" );
+            } 
+        }
+    });
+});
