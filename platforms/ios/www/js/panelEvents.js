3dfedee963ea66ab736f02828c9896c517b361c2
Zach <nicholasbond@stfrancis.edu>
2015-03-05 15:13:40 -0600

    Added swipe tutorial

--- platforms/ios/www/js/panelEvents.js
+++ platforms/ios/www/js/panelEvents.js
@@ -2,6 +2,32 @@ $('.menuButton').on('tap',function(){
 	$('.menupanel').panel("open");
 });
 
+function openTut(){
+	$('.swipePic').css('z-index', 100);
+	$('.dimmer').css('z-index', 98);
+};
+
+function tutorial(){
+    $('.swipePic').animate({'margin-left':'40%'},1000, function(){
+							$( '.menupanel' ).panel( "open" );
+							
+							setTimeout(function(){
+								$('.swipePic').animate({'margin-left':'0%'},1000);
+							}, 1000);	
+							
+							setTimeout(function(){
+								$( '.menupanel' ).panel( "close" );
+							}, 2000);
+                        }
+					);
+};
+
+function closeTut(){
+	$('.swipePic').css('z-index', -10);
+	$('.dimmer').css('z-index', -10);
+};
+
+
 $( document ).on( "pageinit", function() {
     $( document ).on( "swiperight", function( e ) {
         // We check if there is no open panel on the page because otherwise
